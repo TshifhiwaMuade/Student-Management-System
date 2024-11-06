56f1eb9ac77c686ec22a9d5036acd9f4fb5fa13b
GloryBinkatabana <136030784+GloryBinkatabana@users.noreply.github.com>
2024-11-07 01:34:22 +0200

    Update Form1.cs
    The uploading to txt via form on add btn is working

--- Student Management System/Form1.cs
+++ Student Management System/Form1.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,52 @@ namespace Student_Management_System
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\Glory Binkatabana\Documents\PRG 282 Project\Student Management System\Student Details.txt";
+
+
+            // Check if the file is empty or does not exist, then add headers
+            bool isFileEmpty = new FileInfo(path).Length == 0;
+
+            using (FileStream mystream = new FileStream(path, FileMode.Append))
+            using (StreamWriter sw = new StreamWriter(mystream))
+            {
+                // Add headers if the file is empty
+                if (isFileEmpty)
+                {
+                    
+                    sw.WriteLine("Student ID, Name, Age, Course");
+                }
+
+                // Append student data in a new line
+                sw.WriteLine($"{TxtStudentId.Text}, {TxtName.Text}, {TxtAge.Text}, {TxtCourse.Text}");
+            }
+
+            MessageBox.Show("Task completed");
+        }
+
+       
+
+        private void TxtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtAge_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtCourse_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
